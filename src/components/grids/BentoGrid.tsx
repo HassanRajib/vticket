72b283a5b699662e9ed150ea456c660285ff9808
rajib <hasan.rajib39@gmail.com>
2025-05-18 11:48:56 +0600

    lets

--- src/components/grids/BentoGrid.tsx
+++ src/components/grids/BentoGrid.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
-import { cn } from '@/lib/utils'
+
 import { BentoGrid, BentoGridItem } from '../ui/bento-grid'
 import ColourfulText from '../ui/colourful-text'
 
-const Skeleton = () => (
-    <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
-  );
 
 const BentoGri = () => {
     
